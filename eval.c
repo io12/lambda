c39d677e73c8c0e3d78abee4fc6a1e43f7cb835a
io12 <io12@users.noreply.github.com>
2017-08-13 00:17:46 -0400

    update eval.c

--- eval.c
+++ eval.c
@@ -1,10 +1,52 @@
 #include "lambda_calc.h"
 
-static struct expr *beta_reduce(const struct expr *expr)
+// returns true if fully reduced
+static bool beta_reduce(struct expr *expr)
+{
+	struct expr *lambda;
+
+	switch (expr->type) {
+	case VAR:
+		return true;
+	case APP:
+		if (expr->u.app.l->type != LAMBDA) {
+			return beta_reduce(expr->u.app.l) &&
+				beta_reduce(expr->u.app.r);
+		}
+		lambda = expr->u.app.l;
+		substitute(lambda->u.lambda.param_letter,
+				expr->u.app.r, lambda->u.lambda.body);
+		return false;
+	case LAMBDA:
+		return beta_reduce(expr->u.lambda.body);
+	}
+}
+
+// TODO: add calls to free()
+static void substitute(int var_letter, struct expr *replacement, struct expr *expr)
 {
 	switch (expr->type) {
 	case VAR:
+		// [x/a] x = a
+		if (expr->u.var.letter == var_letter) {
+			memcpy(expr, replacement, sizeof(struct expr));
+		}
+		// [x/a] y = y
+		return;
 	case APP:
+		// [x/a] (e e') = ([x/a] e) ([x/a] e')
+		substitute(var_letter, replacement, expr->u.app.l);
+		substitute(var_letter, replacement, expr->u.app.r);
+		return;
 	case LAMBDA:
+		// [x/a] λx.e = λx.e
+		if (expr->u.lambda.param_letter == var_letter) {
+			return;
+		}
+		// [x/a] λy.e = λy.([x/a] e)
+		substitute(var_letter, replacement, expr->u.lambda.body); // TODO: fv
+		return;
+	default:
+		panic("internal error"); // TODO: Is this needed?
 	}
 }
