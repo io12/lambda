011bc2e56fe50497e45a12c01b7087d883f30a49
io12 <io12@users.noreply.github.com>
2017-08-12 21:30:14 -0400

    move parser functions to parse.c

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <ctype.h>
+#include "u.h"
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "lambda_calc.h"
@@ -13,10 +8,6 @@
 #else
 #define NORETURN
 #endif
-
-#define new(type) ((type *) xmalloc(sizeof(type)))
-
-#define TOK_TO_STR(X) (X == EOF ? "EOF" : (char[]){'\'', X, '\'', '\0'})
 
 struct expr {
 	enum { VAR, APP, LAMBDA } type;
@@ -33,85 +24,6 @@ struct expr {
 		} lambda;
 	} u;
 };
-
-struct expr *parse_line(void)
-{
-	return parse_app(EOF);
-}
-
-struct expr *parse_app(const int end_tok)
-{
-	struct expr *expr, *super_expr;
-
-	expr = parse_term();
-	for (;;) {
-		if (peek_tok() == end_tok) {
-			next_tok();
-			return expr;
-		}
-		super_expr = new(struct expr);
-		super_expr->type = APP;
-		super_expr->u.app.l = expr;
-		super_expr->u.app.r = parse_term();
-		expr = super_expr;
-	}
-}
-
-struct expr *parse_term(void)
-{
-	int tok;
-
-	tok = peek_tok();
-	switch (tok) {
-	case '(':
-		return parse_paren_expr();
-	case '\\':
-		return parse_lambda();
-	}
-	if (isalpha(tok)) {
-		return parse_var();
-	}
-	panic("stray token %s", TOK_TO_STR(tok));
-}
-
-struct expr *parse_paren_expr(void)
-{
-	expect_tok('(');
-	return parse_app(')');
-}
-
-struct expr *parse_lambda(void)
-{
-	int param_letter;
-	struct expr *expr;
-
-	expect_tok('\\');
-	param_letter = next_tok();
-	if (!isalpha(param_letter)) {
-		panic("expected a letter, instead recieved %s", TOK_TO_STR(param_letter));
-	}
-	expect_tok('.');
-	expr = new(struct expr);
-	expr->type = LAMBDA;
-	expr->u.lambda.param_letter = param_letter;
-	expr->u.lambda.body = parse_term();
-	return expr;
-}
-
-struct expr *parse_var(void)
-{
-	int var_letter;
-	struct expr *expr;
-
-	var_letter = next_tok();
-	if (!isalpha(var_letter)) {
-		panic("expected a letter, instead recieved %s", TOK_TO_STR(var_letter));
-	}
-	expr = new(struct expr);
-	expr->type = VAR;
-	expr->u.var.letter = var_letter;
-	return expr;
-}
 
 void *xmalloc(const size_t size)
 {
---
+++ parse.c
@@ -0,0 +1,82 @@
+#define new(type) ((type *) xmalloc(sizeof(type)))
+
+#define TOK_TO_STR(X) (X == EOF ? "EOF" : (char[]){'\'', X, '\'', '\0'})
+
+struct expr *parse_line(void)
+{
+	return parse_app(EOF);
+}
+
+struct expr *parse_app(const int end_tok)
+{
+	struct expr *expr, *super_expr;
+
+	expr = parse_term();
+	for (;;) {
+		if (peek_tok() == end_tok) {
+			next_tok();
+			return expr;
+		}
+		super_expr = new(struct expr);
+		super_expr->type = APP;
+		super_expr->u.app.l = expr;
+		super_expr->u.app.r = parse_term();
+		expr = super_expr;
+	}
+}
+
+struct expr *parse_term(void)
+{
+	int tok;
+
+	tok = peek_tok();
+	switch (tok) {
+	case '(':
+		return parse_paren_expr();
+	case '\\':
+		return parse_lambda();
+	}
+	if (isalpha(tok)) {
+		return parse_var();
+	}
+	panic("stray token %s", TOK_TO_STR(tok));
+}
+
+struct expr *parse_paren_expr(void)
+{
+	expect_tok('(');
+	return parse_app(')');
+}
+
+struct expr *parse_lambda(void)
+{
+	int param_letter;
+	struct expr *expr;
+
+	expect_tok('\\');
+	param_letter = next_tok();
+	if (!isalpha(param_letter)) {
+		panic("expected a letter, instead recieved %s", TOK_TO_STR(param_letter));
+	}
+	expect_tok('.');
+	expr = new(struct expr);
+	expr->type = LAMBDA;
+	expr->u.lambda.param_letter = param_letter;
+	expr->u.lambda.body = parse_term();
+	return expr;
+}
+
+struct expr *parse_var(void)
+{
+	int var_letter;
+	struct expr *expr;
+
+	var_letter = next_tok();
+	if (!isalpha(var_letter)) {
+		panic("expected a letter, instead recieved %s", TOK_TO_STR(var_letter));
+	}
+	expr = new(struct expr);
+	expr->type = VAR;
+	expr->u.var.letter = var_letter;
+	return expr;
+}
